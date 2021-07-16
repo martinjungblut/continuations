b9d2f86602fd5b980008a143a4f688082e3c2462
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2021-07-16 18:54:46 -0300

    Add better examples.

--- scheme/continuations.scm
+++ scheme/continuations.scm
@@ -1,5 +1,5 @@
-(define add1 (lambda (n k) (k (+ 1 n))))
-(define mul2 (lambda (n k) (k (* 2 n))))
+;(define add1 (lambda (n k) (k (+ 1 n))))
+;(define mul2 (lambda (n k) (k (* 2 n))))
 
 ;-----
 
@@ -23,27 +23,29 @@
 
 ;-----
 
-(define add1 (lambda (n k) (k (+ 1 n))))
-(define mul5 (lambda (n k) (k (* 5 n))))
+;(define add1 (lambda (n k) (k (+ 1 n))))
+;(define mul5 (lambda (n k) (k (* 5 n))))
 
-(define with-cc
-  (lambda (value expr continuation)
-    (begin
-      (expr continuation)
-      (continuation value))))
+;(define with-cc
+;  (lambda (value expr continuation)
+;    (begin
+;      (expr continuation)
+;      (continuation value))))
+;
+;(define first-continuation #f)
+;(define second-continuation #f)
+;(with-cc 10
+;  (lambda (cont) (set! first-continuation cont))
+;  (lambda (a)
+;    (add1 a (lambda (b)
+;              (begin
+;                (with-cc b
+;                  (lambda (cont) (set! second-continuation cont))
+;                  (lambda (c) (mul5 c (lambda (d) d)))))))))
+;(first-continuation 0)
+;(second-continuation 3)
 
-(define first-continuation #f)
-(define second-continuation #f)
-(with-cc 10
-  (lambda (cont) (set! first-continuation cont))
-  (lambda (a)
-    (add1 a (lambda (b)
-              (begin
-                (with-cc b
-                  (lambda (cont) (set! second-continuation cont))
-                  (lambda (c) (mul5 c (lambda (d) d)))))))))
-(first-continuation 0)
-(second-continuation 3)
+;(add1 10 (lambda (a) (mul5 a (lambda (b) b))))
 
 ;-----
 
@@ -56,11 +58,15 @@
 ;-----
 
 ;; storing a continuation
-;(define x #f)
-;
-;(* 5 (+ 1 (call-with-current-continuation (lambda (cont) (set! x cont) 10))))
-;
-;(x 4)
+(define x #f)
+
+(* 5 (+ 1 (call-with-current-continuation
+           (lambda (cont)
+             (set! x cont)
+             10))))
+
+(x 4)
+(x 3)
 
 ;(define x #f)
 ;(* 5 (+ 1 (call-with-current-continuation (lambda (cont) (begin (set! x cont) (cont 10) 6)))))
