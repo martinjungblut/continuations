15deb38bd18fc37287693b77e5b5eee1686269eb
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2021-10-05 01:50:35 -0300

    Improve Python example.

--- python/example.py
+++ python/example.py
@@ -23,8 +23,16 @@ class ContinuationCapture:
 
 
 capture = ContinuationCapture()
-with_cc(capture, lambda a: add1(a, lambda b: mul5(b, lambda c: print(c))))(10)
+with_cc(capture, lambda a: add1(a, lambda b: mul5(b, lambda c: print(c))))
 
+# same as add1(10, lambda a: mul5(a, print))
+# writes 55 to stdout
+capture.continuation(10)
 
+# same as add1(3, lambda a: mul5(a, print))
+# writes 20 to stdout
 capture.continuation(3)
+
+# same as add1(4, lambda a: mul5(a, print))
+# writes 25 to stdout
 capture.continuation(4)
