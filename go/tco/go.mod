1d124f2e3321e0fc322f972204dc25acdbd6cff5
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-03-25 06:42:25 -0300

    Attempt to improve Go's TCO implementation.

--- go/tco/go.mod
+++ go/tco/go.mod
@@ -1,3 +1,3 @@
 module tco
 
-go 1.16
+go 1.18
--- go/tco/main.go
+++ go/tco/main.go
@@ -2,64 +2,43 @@ package main
 
 import "fmt"
 
-func classicFib(a uint64, b uint64, counter uint64, limit uint64) uint64 {
-	if counter > limit {
-		return a
-	} else if counter == limit {
-		return b
+type capture[T any] struct {
+	continuation func() optional[T]
+}
+
+type optional[T any] struct {
+	value   T
+	capture *capture[T]
+}
+
+func recurse[T any](c func() optional[T]) T {
+	result := c()
+	for {
+		if result.capture != nil {
+			result = result.capture.continuation()
+		} else {
+			break
+		}
+	}
+	return result.value
+}
+
+func fib(a uint64, b uint64, counter uint64, limit uint64) optional[uint64] {
+	if limit == 0 {
+		return optional[uint64]{value: 0}
+	} else if counter < limit {
+		return optional[uint64]{capture: &capture[uint64]{
+			func() optional[uint64] {
+				return fib(b, a+b, counter+1, limit)
+			},
+		}}
 	} else {
-		return classicFib(b, a+b, counter+1, limit)
+		return optional[uint64]{value: b}
 	}
-}
-
-type continuation[T any] struct {
-	funcref func(...T) continuation[T]
-	args    []T
-}
-
-func NewContinuation(funcref func(...interface{}) continuation, args ...interface{}) continuation {
-	return continuation{funcref, args}
-}
-
-type capture struct {
-	called bool
-	values []interface{}
-}
-
-func continuationFib(args ...interface{}) continuation {
-	a := args[0].(uint64)
-	b := args[1].(uint64)
-	counter := args[2].(uint64)
-	limit := args[3].(uint64)
-	doCapture := args[4].(func(...interface{}) continuation)
-
-	if counter > limit {
-		return NewContinuation(doCapture, a)
-	} else if counter == limit {
-		return NewContinuation(doCapture, b)
-	} else {
-		return NewContinuation(continuationFib, b, a+b, counter+1, limit, doCapture)
-	}
-}
-
-func improvedFib(a uint64, b uint64, counter uint64, limit uint64) uint64 {
-	capture := new(capture)
-
-	doCapture := func(values ...interface{}) continuation {
-		capture.called = true
-		capture.values = values
-		return continuation{}
-	}
-
-	nextContinuation := continuationFib(a, b, counter, limit, doCapture)
-	for !capture.called {
-		nextContinuation = nextContinuation.funcref(nextContinuation.args...)
-	}
-
-	return capture.values[0].(uint64)
 }
 
 func main() {
-	// fmt.Printf("%v\n", classicFib(0, 1, 1, 8_000_000))
-	fmt.Printf("%v\n", improvedFib(0, 1, 1, 8_000_000))
+	fmt.Printf("%v\n", recurse(func() optional[uint64] {
+		return fib(0, 1, 1, 2000)
+	}))
 }
