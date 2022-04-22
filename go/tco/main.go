e66c9be6aee9afadc37fa69bab8eebd57d647eeb
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-04-22 03:24:30 -0300

    Shrink Go version down a bit.

--- go/tco/main.go
+++ go/tco/main.go
@@ -5,18 +5,16 @@ import (
 	"math/big"
 )
 
-type continuation[T any] func() result[T]
-
 type result[T any] struct {
 	value        T
-	continuation *continuation[T]
+	continuation func() result[T]
 }
 
-func recurse[T any](continuation continuation[T]) T {
+func recurse[T any](continuation func() result[T]) T {
 	result := continuation()
 	for {
 		if result.continuation != nil {
-			result = (*result.continuation)()
+			result = result.continuation()
 		} else {
 			break
 		}
@@ -24,47 +22,30 @@ func recurse[T any](continuation continuation[T]) T {
 	return result.value
 }
 
-func nextContinuation[T any](continuation continuation[T]) result[T] {
-	return result[T]{continuation: &continuation}
+func nextContinuation[T any](continuation func() result[T]) result[T] {
+	return result[T]{continuation: continuation}
 }
 
 func finalValue[T any](value T) result[T] {
 	return result[T]{value: value}
 }
 
-func fib(a *big.Int, b *big.Int, counter *big.Int, limit *big.Int) result[*big.Int] {
-	zero, one := big.NewInt(0), big.NewInt(1)
+func factorial(n *big.Int, acc *big.Int) func() result[*big.Int] {
+	return func() result[*big.Int] {
+		zero, one := big.NewInt(0), big.NewInt(1)
 
-	if limit.Cmp(zero) == 0 {
-		return finalValue(zero)
-	} else if counter.Cmp(limit) == 0 {
-		return finalValue(b)
-	} else {
-		return nextContinuation(func() result[*big.Int] {
-			return fib(b, a.Add(a, b), counter.Add(counter, one), limit)
-		})
-	}
-}
+		if acc == nil {
+			acc = one
+		}
 
-func factorial(n *big.Int, acc *big.Int) result[*big.Int] {
-	zero, one := big.NewInt(0), big.NewInt(1)
-
-	if acc == nil {
-		acc = big.NewInt(1)
-	}
-
-	if n.Cmp(zero) == 0 || n.Cmp(one) == 0 {
-		return finalValue(acc)
-	} else {
-		return nextContinuation(func() result[*big.Int] {
-			return factorial(new(big.Int).Sub(n, one), acc.Mul(acc, n))
-		})
+		if n.Cmp(zero) == 0 || n.Cmp(one) == 0 {
+			return finalValue(acc)
+		} else {
+			return nextContinuation(factorial(new(big.Int).Sub(n, one), acc.Mul(acc, n)))
+		}
 	}
 }
 
 func main() {
-	fmt.Printf("%v\n", recurse(func() result[*big.Int] {
-		n := big.NewInt(5)
-		return factorial(n, nil)
-	}))
+	fmt.Printf("%v\n", recurse(factorial(big.NewInt(5), nil)))
 }
