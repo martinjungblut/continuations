90124a1626d47e96aff51675c52a0f73a47fa32a
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-04-21 22:49:11 -0300

    Add TCO'd Python factorial.

--- python/tco.py
+++ python/tco.py
@@ -35,5 +35,23 @@ def cps_fibonacci(limit, a=0, b=1, counter=1):
         return cps_fibonacci.recurse(limit, b, b + a, counter + 1)
 
 
-print("CPS-based TCO")
+@enable_tco
+def factorial(n, acc=1):
+    if n in (0, 1):
+        return acc
+    else:
+        return factorial.recurse(n - 1, acc * n)
+
+
+print("Fibonacci")
 print(cps_fibonacci(2000))
+
+
+print("Factorial")
+print(factorial(6))
+print(factorial(5))
+print(factorial(4))
+print(factorial(3))
+print(factorial(2))
+print(factorial(1))
+print(factorial(0))
